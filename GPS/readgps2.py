8c26bd8e640e9eec65319f8514e4e4bc6523b3cd
mehmetulas98 <sherpard3131@gmail.com>
2022-04-06 18:55:22 +0300

    gps düzenlebdi readgps2.py kullanulacak

--- GPS/readgps2.py
+++ GPS/readgps2.py
@@ -2,17 +2,18 @@ import serial
 import time
 import string
 import pynmea2
-
 while True:
 	port="/dev/ttyAMA0"
-	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+	ser=serial.Serial(port, baudrate=9600, timeout=3)
 	dataout = pynmea2.NMEAStreamReader()
-	newdata=ser.readline()
-	print(newdata)
-
-	if newdata[0:6] == "$GPRMC":
+	newdata=str(ser.readline())
+	if newdata[2:7] == "GPGGA":
+		eklenecek = "0000"        
+		newdata=newdata[2:-5]
+		newdata = newdata[:-3] + eklenecek + newdata[-3:]
+		org = "$GPGGA,100506.00,3828.54822,N,02713.19127,E,1,05,2.32,135.4,M,34.7,M,,0000*58"
 		newmsg=pynmea2.parse(newdata)
 		lat=newmsg.latitude
 		lng=newmsg.longitude
-		gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+		gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
 		print(gps)
